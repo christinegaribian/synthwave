4c693fdf4be852271293a8722eea062420702930
Christine <christinegaribian@gmail.com>
2017-05-30 16:50:39 -0700

    Add conditional playing of instruments

--- synthwaves.js
+++ synthwaves.js
@@ -5,11 +5,30 @@ const Bird = require("./lib/bird");
 document.addEventListener("DOMContentLoaded", function(){
   const keyboard = document.getElementById("keyboard");
   const animation = new KeyboardAnimation(keyboard);
+  const selected_sound = document.getElementById("sounds");
+
+  selected_sound.onchange = (e) => {
+    const options = e.srcElement.options;
+    const chosenInstrumentName = options[options.selectedIndex].innerHTML;
+    // if(chosenInstrument){
+    //   chosenInstrument.destroy
+    // }
+
+    // const chosenInstrument = null;
+    switch(chosenInstrumentName){
+      case 'Computer-Generated Synth':
+        return new Synth();
+      case 'Bird':
+        // const bird = new Bird();
+        return new Bird();
+    }
+  }
 
   window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
+
   // const synth = new Synth();
-  const bird = new Bird();
+  // const bird = new Bird();
 
 
 });
