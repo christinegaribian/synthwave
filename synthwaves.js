437269f132cd39b0cbbb3c2251b0ce1692f7da0e
Christine <christinegaribian@gmail.com>
2017-05-30 17:33:02 -0700

    Conditionally play audio based on selected setting. Working correctly now

--- synthwaves.js
+++ synthwaves.js
@@ -6,21 +6,20 @@ document.addEventListener("DOMContentLoaded", function(){
   const keyboard = document.getElementById("keyboard");
   const animation = new KeyboardAnimation(keyboard);
   const selected_sound = document.getElementById("sounds");
+  let chosenInstrument = null;
 
   selected_sound.onchange = (e) => {
     const options = e.srcElement.options;
     const chosenInstrumentName = options[options.selectedIndex].innerHTML;
     if(chosenInstrument){
-      chosenInstrument.destroy
+      chosenInstrument.destroy();
     }
 
-    const chosenInstrument = null;
     switch(chosenInstrumentName){
       case 'Computer-Generated Synth':
-        return new Synth();
+        return chosenInstrument = new Synth();
       case 'Bird':
-        // const bird = new Bird();
-        return new Bird();
+        return chosenInstrument = new Bird();
     }
   }
 
