78fd20d7e2aad35f4d83277fd97aa5e848dbf989
Christine <christinegaribian@gmail.com>
2017-05-30 21:15:36 -0700

    Both Computer-generated synth and plucky synth now correctly playing notes simultaneously

--- synthwaves.js
+++ synthwaves.js
@@ -7,29 +7,24 @@ document.addEventListener("DOMContentLoaded", function(){
   const animation = new KeyboardAnimation(keyboard);
 
   window.AudioContext = window.AudioContext || window.webkitAudioContext;
-  // const selected_sound = document.getElementById("sounds");
-  // let chosenInstrument = null;
+  const selected_sound = document.getElementById("sounds");
+  let chosenInstrument = null;
 
 
-  // selected_sound.onchange = (e) => {
-  //   const options = e.srcElement.options;
-  //   const chosenInstrumentName = options[options.selectedIndex].innerHTML;
-  //   if(chosenInstrument){
-  //     chosenInstrument.destroy();
-  //   }
-  //
-  //   switch(chosenInstrumentName){
-  //     case 'Computer-Generated Synth':
-  //       return chosenInstrument = new Audio();
-  //     case 'Bird':
-  //       return chosenInstrument = new Bird();
-  //   }
-  // }
+  selected_sound.onchange = (e) => {
+    const options = e.srcElement.options;
+    const chosenInstrumentName = options[options.selectedIndex].innerHTML;
+    if(chosenInstrument){
+      chosenInstrument.destroy();
+    }
 
-
-
-  const synth = new Synth();
-  // const bird = new Bird();
-
-
+    switch(chosenInstrumentName){
+      case 'Computer-Generated Synth':
+        return chosenInstrument = new Synth();
+      case 'Bird':
+        return chosenInstrument = new Bird();
+      case 'Plucky Synth':
+        return chosenInstrument = new Synth({isPluckySound: true});
+    }
+  }
 });
