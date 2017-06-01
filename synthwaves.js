e355f737da0753d5f795614b3843f9140ace30f3
Christine <christinegaribian@gmail.com>
2017-06-01 09:22:11 -0700

    Add logic for creating new synth based on which visualizer is chosen

--- synthwaves.js
+++ synthwaves.js
@@ -10,28 +10,50 @@ document.addEventListener("DOMContentLoaded", function(){
   // Audio
   window.AudioContext = window.AudioContext || window.webkitAudioContext;
   const selectedSound = document.getElementById("sounds");
-  let chosenInstrument = new Synth({isPluckySound: true});
+  const selectedVisualization = document.getElementById("visuals");
+  // Audio Visualization
+  window.canvas = document.getElementById("visualization");
+  window.canvasContext = canvas.getContext("2d");
+
+
+
+  let chosenInstrument = new Synth();
+  let visualizer;
 
   selectedSound.onchange = (e) => {
-    const options = e.srcElement.options;
-    const chosenInstrumentName = options[options.selectedIndex].innerHTML;
+    if(e.target.id == 'visuals'){
+      visualizer = e.target.value;
+    } else {
+      // visualizer = 'wavelength';
+      visualizer = selectedVisualization
+                  .options[selectedVisualization.selectedIndex]
+                  .innerHTML.toLowerCase();
+                  console.log(visualizer);
+      // console.log(selectedVisualization.options[selectedVisualization.selectedIndex]);
+      if (!['frequency', 'wavelength'].includes(visualizer)){
+        visualizer = 'frequency';
+      }
+    }
+    const chosenInstrumentName = e.target.value;
     if(chosenInstrument){
       chosenInstrument.destroy();
     }
 
     switch(chosenInstrumentName){
-      case 'Computer-Generated Synth':
-        return chosenInstrument = new Synth();
-      case 'Bird':
+      case 'synth':
+        return chosenInstrument = new Synth(visualizer, false);
+      case 'bird':
         return chosenInstrument = new Bird();
-      case 'Plucky Synth':
-        return chosenInstrument = new Synth({isPluckySound: true});
+      case 'plucky':
+        return chosenInstrument = new Synth(visualizer);
     }
   }
 
-  // Audio Visualization
-  window.canvas = document.getElementById("visualization");
-  window.canvasContext = canvas.getContext("2d");
+  selectedVisualization.onchange = (e) => {
+    selectedSound.onchange(e);
+  }
+
+
 
   // let chosenVisualization = new Visualization({isPluckySound: true});
   //
